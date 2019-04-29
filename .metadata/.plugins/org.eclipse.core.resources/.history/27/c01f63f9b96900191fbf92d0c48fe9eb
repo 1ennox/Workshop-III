21280f92c6554f1af9b81428a74b4a453880a6e3
lennox805 <774146430@qq.com>
2019-04-29 20:21:17 +0800

    fix some bugs by Jim

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/27/c01f63f9b96900191fbf92d0c48fe9eb
@@ -0,0 +1,5 @@
+package brewDay;
+
+public class StorageIngredient {
+
+}
--- brewDay/src/brewDay/StorageIngredient.java
+++ brewDay/src/brewDay/StorageIngredient.java
@@ -1,5 +1,16 @@
 package brewDay;
 
 public class StorageIngredient {
-
+	private int idOfIngredeint;
+	private String nameOfStorageIngredient;
+	private float amountOfStorageIngredient;
+	private char unitOfStorageIngredient;
+	private Brew brew;
+	
+	public StorageIngredient(int idOfIngredient, String nameOfStorageIngredient, char unitOfStorageIngredient) {
+		this.idOfIngredeint = idOfIngredient;
+		this.nameOfStorageIngredient = nameOfStorageIngredient;
+		this.amountOfStorageIngredient = 0;
+		this.unitOfStorageIngredient = unitOfStorageIngredient;
+	}
 }
